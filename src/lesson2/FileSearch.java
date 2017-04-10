025ec479c6545b74edcd76a52d33750a323dc37f
buithuynhung <buithuynhung1997@gmail.com>
2017-04-10 13:19:28 +0300

    Add files via upload

--- src/lesson2/FileSearch.java
+++ src/lesson2/FileSearch.java
@@ -9,36 +9,23 @@ import java.util.List;
 public class FileSearch {
 
     private ArrayList<String> result = new ArrayList<>();
-    private int count;
 
     public ArrayList<String> getResult() {
-        if (count != 0) {
-            for (String str : result) {
-                System.out.println(str);
-            }
-        }
         return result;
     }
 
-    public int getCount() {
-        if (count == 0) System.out.println("Нет такого файла в данной директории!");
-        else System.out.println("\nFound " + count + " result:\n");
-        return count;
-    }
-
-    public void search(String fileName, File directory, boolean flagCmd) throws FileNotFoundException {
+    public void search(String fileName, File directory, boolean searchInSubfolder) throws FileNotFoundException {
 
         if (!directory.isDirectory()) throw new FileNotFoundException();
 
         if (directory.canRead()) {
 
             for (File temp : directory.listFiles()) {
-                if (flagCmd && temp.isDirectory())
-                    search(fileName, temp, flagCmd);
+                if (searchInSubfolder && temp.isDirectory())
+                    search(fileName, temp, searchInSubfolder);
                 else {
                     if (temp.getName().equals(fileName)) {
                         result.add(temp.getAbsoluteFile().toString());
-                        count++;
                     }
                 }
             }
@@ -54,29 +41,40 @@ public class FileSearch {
             System.out.println("Введите нужные вам команды и имя файла, \n" +
                     "[-d] - поиск файла в директории \n" +
                     "[-r] - поиск файла в поддиректриях \n");
-        } else {
-            if (args.length < 1 || args.length > 4) throw new IndexOutOfBoundsException();
-            File directory = new File(new File("").getAbsolutePath());
-
-
-            if (cmdLine.contains("-d")) {
-                String dirName = cmdLine.get(cmdLine.indexOf("-d") + 1).toString();
-                directory = new File(dirName);
-                System.out.println("Директория, в которой нужно проверить наличие файла: " + directory);
-            }
-            String fileName = args[args.length - 1];
-            System.out.println("Имя найденного файла: " + fileName);
-
-            if (!cmdLine.contains("-r")) {
-                fileSearch.search(fileName, directory, false);
-                fileSearch.getCount();
-                fileSearch.getResult();
-            } else {
-                fileSearch.search(fileName, directory, true);
-                fileSearch.getCount();
-                fileSearch.getResult();
-            }
+            return;
         }
+
+        if (args.length < 1 || args.length > 4) throw new IndexOutOfBoundsException();
+        File directory = new File(new File("").getAbsolutePath());
+
+
+        if (cmdLine.contains("-d")) {
+            String dirName = cmdLine.get(cmdLine.indexOf("-d") + 1).toString();
+            directory = new File(dirName);
+            System.out.println("Директория, в которой нужно проверить наличие файла: " + directory);
+        }
+        String fileName = args[args.length - 1];
+
+        try{
+            fileSearch.search(fileName, directory, cmdLine.contains("-r"));
+        } catch (Exception e){
+            System.err.println(e.toString());
+            return;
+        }
+
+        System.out.println("Имя найденного файла: " + fileName);
+
+        ArrayList<String> result = fileSearch.getResult();
+
+        if (result.size() == 0) System.out.println("Нет такого файла в данной директории!");
+        else {
+            System.out.println("\nFound " + result.size() + " result:\n");
+            for (String str : result) {
+                System.out.println(str);
+            }
+
+        }
+
     }
 
     @Override
@@ -84,9 +82,8 @@ public class FileSearch {
         if (this == obj) return true;
         if (obj instanceof FileSearch) {
             FileSearch other = (FileSearch) obj;
-            if (other.count != this.count) return false;
             for (int i = 0; i < this.result.size(); i++) {
-                if (this.result.get(i).equals( other.result.get(i)))
+                if (this.result.get(i).equals(other.result.get(i)))
                     return false;
             }
             return true;
@@ -98,8 +95,8 @@ public class FileSearch {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("{");
-        for (String str: this.result) {
-            sb.append(str).append(", ");
+        for (String str : this.result) {
+            sb.append(str).append("; ");
         }
         sb.delete(sb.length() - 2, sb.length());
         sb.append("}");
@@ -108,6 +105,6 @@ public class FileSearch {
 
     @Override
     public int hashCode() {
-        return Arrays.hashCode(result.toArray()) + count;
+        return Arrays.hashCode(result.toArray());
     }
 }
